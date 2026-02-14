f3be4b77afa4061f26e484fef75143834d7babdd
Hayden Frank <haydog2006@gmail.com>
2026-02-14 00:57:01 -0500

    fix: refactored walls to use one shape

--- main.lua
+++ main.lua
@@ -9,40 +9,19 @@ function love.load()
 	box.f:setRestitution(1)
 	box.f:setUserData("Box")
 	box.f:setFriction(0)
-	box.b:setFixedRotation(true);
+	box.b:setFixedRotation(true)
 
-	image = love.graphics.newImage('gfx/cd.png')
+	image = love.graphics.newImage("gfx/cd.png")
 	love.graphics.setBackgroundColor(1, 1, 1)
 
 	love.window.setTitle("Epic gay thing")
 
 	love.keyboard.setKeyRepeat(true)
 
-	static = {}
-
-	static.roof = {}
-	static.roof.b = love.physics.newBody(world, 200, 0, "static")
-	static.roof.s = love.physics.newRectangleShape(400, 1)
-	static.roof.f = love.physics.newFixture(static.roof.b, static.roof.s)
-	static.roof.f:setUserData("Roof")
-
-	static.wallr = {}
-	static.wallr.b = love.physics.newBody(world, 400, 200, "static")
-	static.wallr.s = love.physics.newRectangleShape(1, 400)
-	static.wallr.f = love.physics.newFixture(static.wallr.b, static.wallr.s)
-	static.wallr.f:setUserData("WallR")
-
-	static.walll = {}
-	static.walll.b = love.physics.newBody(world, 0, 200, "static")
-	static.walll.s = love.physics.newRectangleShape(1, 400)
-	static.walll.f = love.physics.newFixture(static.walll.b, static.walll.s)
-	static.walll.f:setUserData("WallL")
-
-	static.ground = {}
-	static.ground.b = love.physics.newBody(world, 200, 400, "static")
-	static.ground.s = love.physics.newRectangleShape(400, 1)
-	static.ground.f = love.physics.newFixture(static.ground.b, static.ground.s)
-	static.ground.f:setUserData("Ground")
+	walls = {}
+	walls.body = love.physics.newBody(world, 0, 0, "static")
+	walls.shape = love.physics.newChainShape(true, 0, 0, 400, 0, 400, 400, 0, 400)
+	walls.fixture = love.physics.newFixture(walls.body, walls.shape)
 end
 
 function love.keypressed(key)
@@ -66,25 +45,5 @@ function love.update(dt)
 end
 
 function love.draw()
-
-	-- love.graphics.setColor(1, 0, 0)
-    -- love.graphics.polygon("line",
-    --     static.ground.b:getWorldPoints(static.ground.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     static.wallr.b:getWorldPoints(static.wallr.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     static.walll.b:getWorldPoints(static.walll.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     static.roof.b:getWorldPoints(static.roof.s:getPoints())
-    -- )
-    -- love.graphics.polygon("line",
-    --     box.b:getWorldPoints(box.s:getPoints())
-    -- )
-    -- love.graphics.setColor(1, 1, 1)
-
-	-- love.graphics.rectangle("fill", box.b:getX() - 30, box.b:getY() - 30, 60, 60)
 	love.graphics.draw(image, box.b:getX() - 85, box.b:getY() - 37.25, 0, 0.5, 0.5)
 end
